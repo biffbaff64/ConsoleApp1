b78c23e520638ed9890ab8c2622284eedf8a3f21
BiffBaff64 <richikin@gmail.com>
2024-08-08 22:32:28 +0100

    Essential backup

--- Source/MainGame.cs
+++ Source/MainGame.cs
@@ -17,41 +17,44 @@ public class MainGame : ApplicationAdapter
     public override void Create()
     {
         Logger.CheckPoint();
-
+        
         _spriteBatch = new SpriteBatch();
-
-        Logger.CheckPoint();
 
         _camera = new OrthographicCamera();
         _camera.SetToOrtho( false, 640, 480 );
 
-        Logger.CheckPoint();
-
-//        _background = new Texture( Gdx.Files.Internal( "LughLogo.png" ).FileName );
-
-        Logger.Debug( " - finished" );
+        _background = new Texture( Gdx.Files.Internal( "LughLogo.png" ).FileName );
     }
 
     /// <inheritdoc />
     public override void Render()
     {
-//        ScreenUtils.Clear( 0, 0, 0.2f, 1 );
+        ScreenUtils.Clear( Color.Blue );
 
-//        _camera?.Update();
+        _camera?.Update();
 
-//        _spriteBatch?.SetProjectionMatrix( _camera!.Combined );
-//        _spriteBatch?.Begin();
-//        _spriteBatch?.Draw( _background!, 0, 0 );
-//        _spriteBatch?.End();
+        _spriteBatch?.SetProjectionMatrix( _camera!.Combined );
+
+        if ( _background != null )
+        {
+            _spriteBatch?.Begin();
+            _spriteBatch?.Draw( _background!, 0, 0 );
+            _spriteBatch?.End();
+        }
     }
 
-//    /// <inheritdoc />
-//    public override void Pause()
-//    {
-//    }
+    /// <inheritdoc />
+    public override void Pause()
+    {
+    }
 
-//    /// <inheritdoc />
-//    public override void Resume()
-//    {
-//    }
+    /// <inheritdoc />
+    public override void Resume()
+    {
+    }
+
+    /// <inheritdoc />
+    public override void Resize( int width, int height )
+    {
+    }
 }

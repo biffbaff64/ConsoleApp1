19c8ea5fa61a8aee0f8d208bc4ca40e1622af618
BiffBaff64 <richikin@gmail.com>
2025-08-14 23:05:37 +0100

    Add OpenGL vertex attribute binding methods
    Introduce `VertexAttribBindings` with comprehensive OpenGL vertex attribute functions, including support for integer, double, and pointer-based attributes. Additionally, enable more detailed GL context configuration in `DesktopLauncher` and extend helper definitions in `GLBindingsHelpers`.

--- Source/DesktopLauncher.cs
+++ Source/DesktopLauncher.cs
@@ -10,26 +10,25 @@ namespace ConsoleApp1.Source;
 public static class DesktopLauncher
 {
     /// <summary>
-    /// Entry point for the desktop application. Initializes the application configuration
-    /// and launches the main game loop.
+    /// Entry point for the desktop application. Initializes the application
+    /// configuration information and launches the main game loop.
     /// </summary>
     [STAThread]
     private static void Main( string[] args )
     {
         var config = new DesktopGLApplicationConfiguration
         {
-            Title              = "LughSharp Template",
-            VSyncEnabled       = true,
-            WindowWidth        = 480,
-            WindowHeight       = 320,
-            ForegroundFPS      = 60,
-            DisableAudio       = true,
-            Debug              = true,
-            GLProfilingEnabled = true,
-
-//            GLContextMajorVersion = 3,
-//            GLContextMinorVersion = 3,
-//            GLContextRevision     = 0,
+            Title                 = "LughSharp Template",
+            VSyncEnabled          = true,
+            WindowWidth           = 480,
+            WindowHeight          = 320,
+            ForegroundFPS         = 60,
+            DisableAudio          = true,
+            Debug                 = true,
+            GLProfilingEnabled    = true,
+            GLContextMajorVersion = 3,
+            GLContextMinorVersion = 3,
+            GLContextRevision     = 0,
         };
 
         Engine.Api.CheckEnableDevMode().CheckEnableGodMode();
